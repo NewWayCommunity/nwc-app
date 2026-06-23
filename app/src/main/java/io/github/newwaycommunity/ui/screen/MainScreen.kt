e87ae3e4a7cecc062a9e1d16542e71ca5abcbdc3
yalnie <232255780+yalnie@users.noreply.github.com>
2026-06-23 13:24:22 +0300

    FIX

--- app/src/main/java/io/github/newwaycommunity/ui/screen/MainScreen.kt
+++ app/src/main/java/io/github/newwaycommunity/ui/screen/MainScreen.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.net.Uri
-import android.os.Bundle
+import android.os.Build
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.core.*
